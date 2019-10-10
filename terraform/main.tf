72fabb573e03b873b09ea0f318ad90078ee714fe
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-10 23:42:15 +0100

    Complete tutorial on Terraform
    View tutorial here: https://learn.hashicorp.com/terraform/gcp/intro

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,50 @@
 provider "google" {
-  credentials = file("jupiter-ml-alpha-credentials.json")
-  project = "jupiter-ml-alpha"
-  region = "us-central1"
-  zone = "us-central1-c"
+  credentials = file(var.credentials_file)
+  project = var.project
+  region = var.region
+  zone = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "1.1.0"
+
+  network_name = "terraform-vpc-network"
+  project_id   = var.project
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = var.cidrs[0]
+      subnet_region = var.region
+    },
+    {
+      subnet_name   = "subnet-02"
+      subnet_ip     = var.cidrs[1]
+      subnet_region = var.region
+
+      subnet_private_access = "true"
+    },
+  ]
+
+  secondary_ranges = {
+    subnet-01 = []
+    subnet-02 = []
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = var.machine_types[var.environment]
   tags = ["web", "dev"]
+
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
 
   boot_disk {
     initialize_params {
@@ -21,8 +53,10 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = module.network.network_name
+    subnetwork = module.network.subnets_names[0]
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+project = "jupiter-ml-alpha"
+credentials_file = "jupiter-ml-alpha-credentials.json"
+cidrs = [ "10.0.0.0/16", "10.1.0.0/16" ]
