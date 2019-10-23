de5dc1374880f918b5c70d96e2d9e47b578b6b94
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-23 16:06:31 +0100

    Switch terraform deploy to circle ci

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,17 +25,17 @@ provider "google" {
 //  source = "${path.root}/sns-to-pubsub.zip"
 //}
 
-resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
-  name = "sns-to-pubsub"
-  description = "receive from sns and send to pub sub"
-  available_memory_mb = 128
-  source_archive_bucket = "sns-to-pubsub-bucket"
-  source_archive_object = "sns_to_pubsub_${var.deploy_code_commit_hash}.zip"
-  timeout = 60
-  entry_point = "receiveNotification"
-  trigger_http = true
-  runtime = "nodejs10"
-}
+//resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
+//  name = "sns-to-pubsub"
+//  description = "receive from sns and send to pub sub"
+//  available_memory_mb = 128
+//  source_archive_bucket = "sns-to-pubsub-bucket"
+//  source_archive_object = "sns_to_pubsub_${var.deploy_code_commit_hash}.zip"
+//  timeout = 60
+//  entry_point = "receiveNotification"
+//  trigger_http = true
+//  runtime = "nodejs10"
+//}
 
 ############## sns to pub-sub ends: ###############
 
