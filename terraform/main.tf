9f337d49b6c5d21194d00981e3cfe9efb3231efa
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-23 16:29:11 +0100

    Test circle ci for python functions

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,17 +25,17 @@ provider "google" {
 //  source = "${path.root}/sns-to-pubsub.zip"
 //}
 
-resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
-  name = "sns-to-pubsub"
-  description = "receive from sns and send to pub sub"
-  available_memory_mb = 128
-  source_archive_bucket = "sns-to-pubsub-bucket"
-  source_archive_object = "sns_to_pubsub_${var.deploy_code_commit_hash}.zip"
-  timeout = 60
-  entry_point = "receiveNotification"
-  trigger_http = true
-  runtime = "nodejs10"
-}
+//resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
+//  name = "sns-to-pubsub"
+//  description = "receive from sns and send to pub sub"
+//  available_memory_mb = 128
+//  source_archive_bucket = "sns-to-pubsub-bucket"
+//  source_archive_object = "sns_to_pubsub_${var.deploy_code_commit_hash}.zip"
+//  timeout = 60
+//  entry_point = "receiveNotification"
+//  trigger_http = true
+//  runtime = "nodejs10"
+//}
 
 ############## sns to pub-sub ends: ###############
 
@@ -60,17 +60,17 @@ resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
 //  source = "${path.root}/fetch-from-big-query.zip"
 //}
 
-resource "google_cloudfunctions_function" "fetch-from-big-query-function" {
-  name = "fetch-from-big-query"
-  description = "fetch data from big query"
-  available_memory_mb = 128
-  source_archive_bucket = "fetch-from-big-query-bucket"
-  source_archive_object = "fetch_from_big_query_${var.deploy_code_commit_hash}.zip"
-  timeout = 60
-  entry_point = "fetchFromBigQuery"
-  trigger_http = true
-  runtime = "nodejs10"
-}
+//resource "google_cloudfunctions_function" "fetch-from-big-query-function" {
+//  name = "fetch-from-big-query"
+//  description = "fetch data from big query"
+//  available_memory_mb = 128
+//  source_archive_bucket = "fetch-from-big-query-bucket"
+//  source_archive_object = "fetch_from_big_query_${var.deploy_code_commit_hash}.zip"
+//  timeout = 60
+//  entry_point = "fetchFromBigQuery"
+//  trigger_http = true
+//  runtime = "nodejs10"
+//}
 
 ############## fetch-from-big-query end: ###############
 
@@ -94,19 +94,19 @@ resource "google_cloudfunctions_function" "fetch-from-big-query-function" {
 //  source = "${path.root}/sync-amplitude-data-to-big-query.zip"
 //}
 //
-//resource "google_cloudfunctions_function" "sync-amplitude-data-to-big-query-function" {
-//  name = "sync-amplitude-data-to-big-query"
-//  description = "sync data from Amplitude into Big Query"
-//  available_memory_mb = 128
-//  source_archive_bucket = "sync-amplitude-data-to-big-query-bucket"
-//  source_archive_object = google_storage_bucket_object.sync-amplitude-data-to-big-query-zip.name
-//  timeout = 360
-//  entry_point = "main"
-//  event_trigger {
-//    event_type = "google.pubsub.topic.publish"
-//    resource = "projects/jupiter-ml-alpha/topics/daily-runs"
-//  }
-//  runtime = "python37"
-//}
+resource "google_cloudfunctions_function" "sync-amplitude-data-to-big-query-function" {
+  name = "sync-amplitude-data-to-big-query"
+  description = "sync data from Amplitude into Big Query"
+  available_memory_mb = 128
+  source_archive_bucket = "sync-amplitude-data-to-big-query-bucket"
+  source_archive_object = "sync_amplitude_data_to_big_query_${var.deploy_code_commit_hash}.zip"
+  timeout = 360
+  entry_point = "main"
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource = "projects/jupiter-ml-alpha/topics/daily-runs"
+  }
+  runtime = "python37"
+}
 
 ############## sync-amplitude-data-to-big-query end: ###############
