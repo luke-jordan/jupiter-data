df2910d571656d5ec0b3d91117f1a223d3d9236c
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-28 17:51:43 +0100

    Make `pubsub-to-big-query-for-sns` a push instead of a pull
    Being a `push` subscription means it is called when there is a message
    on pubsub topic `sns-events`. As we project that there would be a low
    volume of messages on pubsub for the moment, this method was chosen over
    a pull because a pull maintains an open connection to pubsub and is
    better for streaming data.

--- functions/python/pubsub-to-big-query-for-sns/main.py
+++ functions/python/pubsub-to-big-query-for-sns/main.py
@@ -1,18 +1,10 @@
 import os
 import json
+import base64
 
-from google.cloud import pubsub_v1
 from google.cloud import bigquery
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="jupiter_ml_python_credentials.json"
-
 project_id = "jupiter-ml-alpha"
-subscription_name = "sns-watcher-v2"
-
-subscriber = pubsub_v1.SubscriberClient()
-subscription_path = subscriber.subscription_path(
-    project_id, subscription_name)
-
 
 client = bigquery.Client()
 dataset_id = 'ops'
@@ -20,25 +12,19 @@ table_id = 'sns_events'
 table_ref = client.dataset(dataset_id).table(table_id)
 table = client.get_table(table_ref)
 
-def callback(message):
-    print("In callback, received raw message here: {message}".format(message=message))
+def main(event, context):
+    print("message received from pubsub")
+
     try:
-        print("decoding raw message")
-        parsed_message = eval(message.data.decode("utf-8"))
-        print("successfully decoded raw message {msg}".format(msg=parsed_message))
-        errors = client.insert_rows(table, parsed_message)
-        print("done with big query insert, about to acknowledge message to pub/sub")
+        print("decoding raw message 'data' from event: {evt}".format(evt=event))
+        message = eval(base64.b64decode(event['data']).decode('utf-8'))
+        print("successfully decoded message: {msg}".format(msg=message))
+        errors = client.insert_rows(table, message)
+        print("successfully inserted message: {msg} into table: {table} of big query".format(msg=message, table=table_id))
         assert errors == []
-#         message.ack()
+        print("acknowledging message to pub/sub")
+        return 'OK', 200
     except AssertionError:
         print('error inserting message with message id: ', errors)
     except Exception as e:
         print('error decoding message on {}' .format(e))
-
-future = subscriber.subscribe(subscription_path, callback=callback)
-
-try:
-    # When timeout is unspecified, the result method waits indefinitely.
-    future.result(timeout=60)
-except Exception as e:
-    print('Listening for messages on {} threw an Exception: {}.'.format(subscription_name, e))
