5cdd21dea8a045a5f412b8209f6bf4695b8dfbbd
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-10-28 17:57:41 +0100

    Separate cloud functions config into separate terraform files
    This helps with long term maintainability. Code is modular, easier to
    manage and reason about

---
+++ terraform/pubsub-to-big-query-for-sns.tf
@@ -0,0 +1,44 @@
+//############## pubsub-to-big-query-for-sns begin: ###############
+//data "archive_file" "pubsub-to-big-query-for-sns-zip" {
+//  type = "zip"
+//  source_dir = "../functions/python/pubsub-to-big-query-for-sns"
+//  output_path = "${path.root}/pubsub-to-big-query-for-sns.zip"
+//}
+//
+//# place the zip-ed code in the bucket
+//resource "google_storage_bucket_object" "pubsub-to-big-query-for-sns-zip" {
+//  name = "pubsub-to-big-query-for-sns.zip"
+//  bucket = "pubsub-to-big-query-for-sns-bucket"
+//  source = "${path.root}/pubsub-to-big-query-for-sns.zip"
+//}
+//
+//resource "google_cloudfunctions_function" "pubsub-to-big-query-for-sns-function" {
+//  name = "pubsub-to-big-query-for-sns"
+//  description = "Fetch Data from Pub/Sub and load into Big Query"
+//  available_memory_mb = 128
+//  source_archive_bucket = "pubsub-to-big-query-for-sns-bucket"
+//  source_archive_object = "pubsub-to-big-query-for-sns.zip"
+//  timeout = 120
+//  entry_point = "main"
+//  event_trigger {
+//    event_type = "google.pubsub.topic.publish"
+//    resource = "projects/jupiter-ml-alpha/topics/sns-events"
+//  }
+//  runtime = "python37"
+//}
+
+
+resource "google_cloudfunctions_function" "pubsub-to-big-query-for-sns-function" {
+  name = "pubsub-to-big-query-for-sns"
+  description = "Fetch Data from Pub/Sub and load into Big Query"
+  available_memory_mb = 128
+  source_archive_bucket = "pubsub-to-big-query-for-sns-bucket"
+  source_archive_object = "pubsub_to_big_query_for_sns_${var.deploy_code_commit_hash}.zip"
+  timeout = 120
+  entry_point = "main"
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource = "projects/jupiter-ml-alpha/topics/sns-events"
+  }
+  runtime = "python37"
+}
---
+++ terraform/sns-to-pubsub.tf
@@ -0,0 +1,32 @@
+//############## sns to pub-sub below: ###############
+//# zip up our source code
+//data "archive_file" "sns-to-pubsub-zip" {
+//  type = "zip"
+//  source_dir = "../functions/javascript/sns-to-pubsub"
+//  output_path = "${path.root}/sns-to-pubsub.zip"
+//}
+////
+//# create the storage bucket
+//resource "google_storage_bucket" "sns-to-pubsub-bucket" {
+//  name = "sns-to-pubsub-bucket"
+//}
+//
+//# place the zip-ed code in the bucket
+//resource "google_storage_bucket_object" "sns-to-pubsub-zip" {
+//  name = "sns-to-pubsub.zip"
+//  bucket = "sns-to-pubsub-bucket"
+//  source = "${path.root}/sns-to-pubsub.zip"
+//}
+
+resource "google_cloudfunctions_function" "sns-to-pubsub-function" {
+  name = "sns-to-pubsub"
+  description = "receive from sns and send to pub sub"
+  available_memory_mb = 128
+  source_archive_bucket = "sns-to-pubsub-bucket"
+  source_archive_object = "sns_to_pubsub_${var.deploy_code_commit_hash}.zip"
+  timeout = 60
+  entry_point = "receiveNotification"
+  trigger_http = true
+  runtime = "nodejs10"
+}
+############## sns to pub-sub ends: ###############
