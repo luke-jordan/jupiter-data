dbe1d2b363474cd8d25f2c90dd4155da64560cbc
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-11-22 13:25:24 +0100

    Format payload for `user_behaviour` table

--- functions/python/user-behaviour/user-behaviour.py
+++ functions/python/user-behaviour/user-behaviour.py
@@ -12,24 +12,92 @@ table_id = 'user_behaviour'
 table_ref = client.dataset(dataset_id).table(table_id)
 table = client.get_table(table_ref)
 
-def logUserTransaction(event, context):
+def missingParameterInPayload (payload): 
+  if ("context" not in payload):
+    print("context not in payload")
+    return True
+  
+  extractedContext = payload["context"]
+
+  if ("accountId" not in extractedContext):
+    print("accountId not in extracted context")
+    return True
+
+  if ("savedAmount" not in extractedContext):
+    print("savedAmount not in extracted context")
+    return True
+
+  if ("timeInMillis" not in extractedContext):
+    print("timeInMillis not in extracted context")
+    return True 
+
+  return False
+
+
+def extractAmountAndCurrency(savedAmount):
+    print("extract amount and currency from savedAmount: {savedAmount}".format(savedAmount=savedAmount))
+    amountBrokenIntoParts = savedAmount.split("::")
+    print("amount broken into parts: ", amountBrokenIntoParts)
+
+    return {
+        "amount": float(amountBrokenIntoParts[0]),
+        "currency": amountBrokenIntoParts[2],
+    }
+
+def formatPayloadForUserBehaviourTable(payloadList):
+    formattedPayloadList = []
+    for eventMessage in payloadList:
+        if (missingParameterInPayload(eventMessage)):
+            print("a required parameter is missing")
+            break
+        
+        context = eventMessage["context"]
+        amountAndCurrency = extractAmountAndCurrency(context["savedAmount"])
+
+        singleFormattedPayload = {
+            "user_id": eventMessage["user_id"],
+            "account_id": context["accountId"],
+            "transaction_type": "deposit",
+            "amount": amountAndCurrency["amount"],
+            "currency": amountAndCurrency["currency"],
+            "time_transaction_occurred": context["timeInMillis"]
+        }
+
+        formattedPayloadList.append(singleFormattedPayload)
+    
+    print("contructed formatted payload list: ", formattedPayloadList)
+    return formattedPayloadList
+
+
+def insertRowsIntoUserBehaviourTable(formattedPayloadList):
+    if (len(formattedPayloadList > 0)):
+        print("inserting formatted payload {msg} into table: {table} of big query".format(msg=formattedPayloadList, table=table_id))
+        try:
+            errors = client.insert_rows(table, formattedPayloadList)
+            print("successfully inserted formatted payload: {msg} into table: {table} of big query".format(msg=formattedPayloadList, table=table_id))
+            assert errors == []
+        except AssertionError:
+            print('error inserting message with message id: ', errors)
+        except Exception as e:
+            print('error decoding message on {}' .format(e))
+
+def decodePubSubMessage(event):
+    print("decoding raw message 'data' from event: {evt}".format(evt=event))
+    msg = eval(base64.b64decode(event['data']).decode('utf-8'))
+    print("successfully decoded message from pubsub. Message: {msg}".format(msg=msg))
+    return msg
+
+
+def formatPayloadAndLogAccountAccountTransaction(event, context):
     print("message received from pubsub")
 
     try:
-        print("decoding raw message 'data' from event: {evt}".format(evt=event))
-        message = eval(base64.b64decode(event['data']).decode('utf-8'))
-        print("successfully decoded message: {msg}".format(msg=message))
-
-        # TODO 2a) define schema for user-behaviour and create table in big query
-
-        # TODO 2b) format message for user-behaviour table
-        errors = client.insert_rows(table, message)
-        print("successfully inserted message: {msg} into table: {table} of big query".format(msg=message, table=table_id))
-        assert errors == []
+        messageFromPubSub = decodePubSubMessage(event)
+        formattedPayloadList = formatPayloadForUserBehaviourTable(messageFromPubSub)
+        
+        insertRowsIntoUserBehaviourTable(formattedPayloadList)
         print("acknowledging message to pub/sub")
         return 'OK', 200
-    except AssertionError:
-        print('error inserting message with message id: ', errors)
     except Exception as e:
         print('error decoding message on {}' .format(e))
 
