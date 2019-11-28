0e1c0921f6333770778cf430c94fedf92e886d3a
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-11-28 19:07:13 +0100

    Add tests for `email-client`

---
+++ functions/javascript/notification-service/test/email-client.test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const config = require('config');
+const shortid = require('shortid');
+const proxyquire = require('proxyquire').noCallThru();
+chai.use(require('sinon-chai'));
+
+const transporterSendMailStub = sinon.stub();
+const MockNodemailer = {
+    createTransport: () => ({
+        'sendMail': transporterSendMailStub
+    })
+};
+
+const emailClientService = proxyquire('../email-client', {
+    'nodemailer': MockNodemailer
+});
+
+const sampleMessage = 'Winter is coming';
+const sampleEmail = ['johnsnow@gmail.com'];
+const sampleRequestId = shortid.generate();
+const {
+    from,
+    subject
+} = config.get("mailFormat");
+
+const resetStubs = () => {
+    transporterSendMailStub.reset();
+};
+
+describe('Notification Service', () => {
+    beforeEach(() => {
+        resetStubs();
+    });
+
+    it('should send an email successfully', async () => {
+        const params = {
+            from,
+            to: sampleEmail,
+            subject,
+            text: sampleMessage,
+            html: sampleMessage
+        };
+        transporterSendMailStub.withArgs().resolves(params);
+        const result = await emailClientService.sendEmail(sampleEmail, sampleMessage, sampleRequestId);
+        expect(result).to.be.undefined;
+        expect(transporterSendMailStub).to.have.been.calledWith(params);
+    });
+
+    it('should handle errors gracefully when sending an email', async () => {
+        const params = {
+            from,
+            to: sampleEmail,
+            subject,
+            text: sampleMessage,
+            html: sampleMessage
+        };
+        transporterSendMailStub.withArgs().rejects('Error while sending email');
+        const result = await emailClientService.sendEmail(sampleEmail, sampleMessage, sampleRequestId);
+        expect(result).to.be.undefined;
+        expect(transporterSendMailStub).to.have.been.calledWith(params);
+    });
+});
