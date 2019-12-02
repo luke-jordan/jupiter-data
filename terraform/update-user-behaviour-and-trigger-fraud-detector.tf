5989444c616a98f89868ba082383ed447b4dd202
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-12-02 19:04:27 +0100

    Fix issue in teeraform config `user-behaviour` script

--- terraform/update-user-behaviour-and-trigger-fraud-detector.tf
+++ terraform/update-user-behaviour-and-trigger-fraud-detector.tf
@@ -3,7 +3,7 @@ resource "google_cloudfunctions_function" "update-user-behaviour-and-trigger-fra
   description = "Updates the user behaviour and triggers fraud detector to run a check on said user"
   available_memory_mb = 128
   source_archive_bucket = "${var.gcp_bucket_prefix[terraform.workspace]}-user-behaviour-bucket"
-  source_archive_object = "user_behaviour_{var.deploy_code_commit_hash}.zip"
+  source_archive_object = "user_behaviour_${var.deploy_code_commit_hash}.zip"
   timeout = 120
   entry_point = "updateUserBehaviourAndTriggerFraudDetector"
   event_trigger {
