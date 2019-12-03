46aa550883836d876b59ee263488305c5d525731
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2019-12-03 18:14:07 +0100

    Fix function naming conflict

--- terraform/fetch-user-behaviour-based-on-rules.tf
+++ terraform/fetch-user-behaviour-based-on-rules.tf
@@ -1,6 +1,6 @@
-resource "google_cloudfunctions_function" "update-user-behaviour-and-trigger-fraud-detector-function" {
-  name = "update-user-behaviour-and-trigger-fraud-detector"
-  description = "Updates the user behaviour and triggers fraud detector to run a check on said user"
+resource "google_cloudfunctions_function" "fetch-user-behaviour-based-on-rules-function" {
+  name = "fetch-user-behaviour-based-on-rules"
+  description = "Fetch User Behaviour based on predefined rules"
   available_memory_mb = 128
   source_archive_bucket = "${var.gcp_bucket_prefix[terraform.workspace]}-user-behaviour-bucket"
   source_archive_object = "user_behaviour_${var.deploy_code_commit_hash}.zip"
