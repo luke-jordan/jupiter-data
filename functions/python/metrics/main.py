56329fb63a84d3e64273b96a8de98b810c48ac2c
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2020-01-15 20:38:11 +0100

    Continued updating tests/functions for metrics

--- functions/python/metrics/main.py
+++ functions/python/metrics/main.py
@@ -1,4 +1,5 @@
 import os
+import constant
 
 from google.cloud import bigquery
 from dotenv import load_dotenv
@@ -11,13 +12,26 @@ client = bigquery.Client()
 project_id = os.getenv("GOOGLE_PROJECT_ID")
 BIG_QUERY_DATASET_LOCATION =  os.getenv("BIG_QUERY_DATASET_LOCATION")
 dataset_id = 'ops'
-table_id = 'user_behaviour'
-table_ref = client.dataset(dataset_id).table(table_id)
-table = client.get_table(table_ref)
+user_behaviour_table_id = 'user_behaviour'
+all_user_events_id = 'all_user_events'
+user_behaviour_table_ref = client.dataset(dataset_id).table(user_behaviour_table_id)
+all_user_events_table_ref = client.dataset(dataset_id).table(all_user_events_id)
 
-FULL_TABLE_URL="{project_id}.{dataset_id}.{table_id}".format(project_id=project_id, dataset_id=dataset_id, table_id=table_id)
-SAVING_EVENT_TRANSACTION_TYPE = 'SAVING_EVENT'
-WITHDRAWAL_TRANSACTION_TYPE = 'WITHDRAWAL'
+USER_BEHAVIOUR_TABLE_URL="{project_id}.{dataset_id}.{table_id}".format(project_id=project_id, dataset_id=dataset_id, table_id=user_behaviour_table_id)
+ALL_USER_EVENTS_TABLE_URL="{project_id}.{dataset_id}.{table_id}".format(project_id=project_id, dataset_id=dataset_id, table_id=all_user_events_table_ref)
+
+FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY = constant.FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY
+SAVING_EVENT_TRANSACTION_TYPE = constant.SAVING_EVENT_TRANSACTION_TYPE
+WITHDRAWAL_TRANSACTION_TYPE = constant.WITHDRAWAL_TRANSACTION_TYPE
+USER_OPENED_APP_EVENT_CODE = constant.USER_OPENED_APP_EVENT_CODE
+EXTERNAL_EVENT_SOURCE = constant.EXTERNAL_EVENT_SOURCE
+INTERNAL_EVENT_SOURCE = constant.INTERNAL_EVENT_SOURCE
+ENTERED_SAVINGS_FUNNEL_EVENT_CODE = constant.ENTERED_SAVINGS_FUNNEL_EVENT_CODE
+ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE = constant.ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE
+
+def convert_amount_from_hundredth_cent_to_whole_currency(amount):
+    print("Converting amount from 'hundredth cent' to 'whole currency'. Amount: {}".format(amount))
+    return float(amount) * FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY
 
 def extract_key_value_from_first_item_of_big_query_response(responseList, key):
     if responseList and len(responseList) > 0:
@@ -35,7 +49,7 @@ def fetch_data_as_list_from_user_behaviour_table(query, query_params):
     print(
         """
         Fetching from table: {table} with query: {query} and params {query_params}
-        """.format(query=query, query_params=query_params, table=table_id)
+        """.format(query=query, query_params=query_params, table=user_behaviour_table_id)
     )
     job_config = bigquery.QueryJobConfig()
     job_config.query_parameters = query_params
@@ -49,20 +63,20 @@ def fetch_data_as_list_from_user_behaviour_table(query, query_params):
     print(
         """
         Successfully fetched from table: {table} with query: {query} and params {query_params}.
-        """.format(query=query, query_params=query_params, table=table_id)
+        """.format(query=query, query_params=query_params, table=user_behaviour_table_id)
     )
 
     return convert_big_query_response_to_list(query_result)
 
 def fetch_total_amount_using_transaction_type(transaction_type, config):
+    least_time_to_consider = config["least_time_to_consider"]
+
     print(
         """
-        Fetching the total amount using transaction type: {transaction_type}.
+        Fetching the total amount using transaction type: {transaction_type} after time: {given_time}
         """
-            .format(transaction_type=transaction_type)
+            .format(transaction_type=transaction_type, given_time=least_time_to_consider)
     )
-
-    least_time_to_consider = config["least_time_to_consider"]
 
     query = (
         """
@@ -70,7 +84,7 @@ def fetch_total_amount_using_transaction_type(transaction_type, config):
         from `{full_table_url}`
         where `transaction_type` = @transactionType
         and `time_transaction_occurred` >= @givenTime 
-        """.format(full_table_url=FULL_TABLE_URL)
+        """.format(full_table_url=USER_BEHAVIOUR_TABLE_URL)
     )
 
     query_params = [
@@ -78,27 +92,159 @@ def fetch_total_amount_using_transaction_type(transaction_type, config):
         bigquery.ScalarQueryParameter("givenTime", "INT64", least_time_to_consider),
     ]
 
-    bigQueryResponse = fetch_data_as_list_from_user_behaviour_table(query, query_params)
-    totalAmount = extract_key_value_from_first_item_of_big_query_response(bigQueryResponse, 'totalAmount')
+    big_query_response = fetch_data_as_list_from_user_behaviour_table(query, query_params)
+    total_amount_in_hundredth_cent = extract_key_value_from_first_item_of_big_query_response(big_query_response, 'totalAmount')
+    total_amount_in_whole_currency = convert_amount_from_hundredth_cent_to_whole_currency(total_amount_in_hundredth_cent)
+    return total_amount_in_whole_currency
 
-    return totalAmount
-
-def fetch_total_saved_amount_during_period(period):
+def fetch_total_saved_amount_since_given_time(time_in_milliseconds):
     return fetch_total_amount_using_transaction_type(
         SAVING_EVENT_TRANSACTION_TYPE,
         {
-            "least_time_to_consider": period,
+            "least_time_to_consider": time_in_milliseconds,
         }
     )
 
-def fetch_total_withdrawn_amount_during_period(period):
+def fetch_total_withdrawn_amount_given_time(time_in_milliseconds):
     return fetch_total_amount_using_transaction_type(
         WITHDRAWAL_TRANSACTION_TYPE,
         {
-            "least_time_to_consider": period,
+            "least_time_to_consider": time_in_milliseconds,
         }
     )
 
+def fetch_count_of_users_that_performed_transaction_type(transaction_type, config):
+    least_time_to_consider = config["least_time_to_consider"]
+
+    print(
+        """
+        Fetching the count of users that performed transaction type: {transaction_type} after time: {given_time}
+        """
+            .format(transaction_type=transaction_type, given_time=least_time_to_consider)
+    )
+
+    query = (
+        """
+        select count(distinct(`user_id`)) as `countOfUsersThatPerformedTransactionType`
+        from `{full_table_url}`
+        where `transaction_type` = @transactionType
+        and `time_transaction_occurred` >= @givenTime
+        """.format(full_table_url=USER_BEHAVIOUR_TABLE_URL)
+    )
+
+    query_params = [
+        bigquery.ScalarQueryParameter("transactionType", "STRING", transaction_type),
+        bigquery.ScalarQueryParameter("givenTime", "INT64", least_time_to_consider),
+    ]
+
+    big_query_response = fetch_data_as_list_from_user_behaviour_table(query, query_params)
+    user_count = extract_key_value_from_first_item_of_big_query_response(big_query_response, 'countOfUsersThatPerformedTransactionType')
+    return user_count
+
+def fetch_count_of_users_that_saved_since_given_time(time_in_milliseconds):
+    return fetch_count_of_users_that_performed_transaction_type(
+        SAVING_EVENT_TRANSACTION_TYPE,
+        {
+            "least_time_to_consider": time_in_milliseconds,
+        }
+    )
+
+def fetch_count_of_users_that_withdrew_since_given_time(time_in_milliseconds):
+    return fetch_count_of_users_that_performed_transaction_type(
+        WITHDRAWAL_TRANSACTION_TYPE,
+        {
+            "least_time_to_consider": time_in_milliseconds,
+        }
+    )
+
+def fetch_count_of_users_that_performed_event(config):
+    event_type = config["event_type"]
+    source_of_event = config["source_of_event"]
+    least_time_to_consider = config["least_time_to_consider"]
+
+    print(
+        """
+        Fetching the count of users that performed event type: {event_type}
+         with source of event: {source_of_event} after time: {given_time}
+        """
+            .format(event_type=event_type, source_of_event=source_of_event, given_time=least_time_to_consider)
+    )
+
+    query = (
+        """
+        select count(distinct(`user_id`)) as `countOfUsersThatPerformedEvent`
+        from `{full_table_url}`
+        and `event_type` = @eventType
+        and `source_of_event` = @sourceOfEvent
+        and `time_transaction_occurred` >= @givenTime 
+        """.format(full_table_url=ALL_USER_EVENTS_TABLE_URL)
+    )
+
+    query_params = [
+        bigquery.ScalarQueryParameter("givenTime", "INT64", least_time_to_consider),
+        bigquery.ScalarQueryParameter("eventType", "STRING", event_type),
+        bigquery.ScalarQueryParameter("sourceOfEvent", "STRING", source_of_event),
+    ]
+
+    big_query_response = fetch_data_as_list_from_user_behaviour_table(query, query_params)
+    user_count = extract_key_value_from_first_item_of_big_query_response(big_query_response, 'countOfUsersThatPerformedEvent')
+    return user_count
+
+def fetch_count_of_users_that_entered_app_since_given_time(time_in_milliseconds):
+    return fetch_count_of_users_that_performed_event(
+        {
+            "event_type": USER_OPENED_APP_EVENT_CODE,
+            "source_of_event": EXTERNAL_EVENT_SOURCE,
+            "least_time_to_consider": time_in_milliseconds,
+        }
+    )
+
+def fetch_total_number_of_users():
+    print(
+        """
+        Fetching the total number of users in app
+        """
+    )
+
+    query = (
+        """
+        select count(distinct(`user_id`)) as `totalNumberOfUsers`
+        from `{full_table_url}`
+        and `source_of_event` = @sourceOfEvent
+        """.format(full_table_url=ALL_USER_EVENTS_TABLE_URL)
+    )
+
+    query_params = [
+        bigquery.ScalarQueryParameter("sourceOfEvent", "STRING", INTERNAL_EVENT_SOURCE),
+    ]
+
+    big_query_response = fetch_data_as_list_from_user_behaviour_table(query, query_params)
+    user_count = extract_key_value_from_first_item_of_big_query_response(big_query_response, 'totalNumberOfUsers')
+    return user_count
+
+def fetch_count_of_users_that_tried_saving(time_in_milliseconds):
+    return fetch_count_of_users_that_performed_event(
+        {
+            "event_type": ENTERED_SAVINGS_FUNNEL_EVENT_CODE,
+            "source_of_event": EXTERNAL_EVENT_SOURCE,
+            "least_time_to_consider": time_in_milliseconds,
+        }
+    )
+
+def fetch_count_of_users_that_tried_withdrawing(time_in_milliseconds):
+    return fetch_count_of_users_that_performed_event(
+        {
+            "event_type": ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE,
+            "source_of_event": EXTERNAL_EVENT_SOURCE,
+            "least_time_to_consider": time_in_milliseconds,
+        }
+    )
+
+def calculate_ratio_of_users_that_entered_app_today_versus_total_users(time_in_milliseconds):
+    return fetch_count_of_users_that_entered_app_since_given_time(time_in_milliseconds) / fetch_total_number_of_users()
+
+def calculate_ratio_of_users_that_saved_versus_users_that_tried_saving(time_in_milliseconds):
+    return fetch_count_of_users_that_saved_since_given_time(time_in_milliseconds) / fetch_count_of_users_that_tried_saving(time_in_milliseconds)
 
 def fetch_daily_metrics():
     return
--- functions/python/metrics/main_test.py
+++ functions/python/metrics/main_test.py
@@ -10,27 +10,69 @@ from main \
     convert_big_query_response_to_list, \
     extract_key_value_from_first_item_of_big_query_response, \
     fetch_total_amount_using_transaction_type, \
-    fetch_total_saved_amount_during_period, \
-    fetch_total_withdrawn_amount_during_period, \
+    fetch_total_saved_amount_since_given_time, \
+    fetch_total_withdrawn_amount_given_time, \
+    convert_amount_from_hundredth_cent_to_whole_currency, \
+    fetch_count_of_users_that_performed_transaction_type, \
+    fetch_count_of_users_that_saved_since_given_time, \
+    fetch_count_of_users_that_withdrew_since_given_time, \
+    fetch_count_of_users_that_performed_event, \
+    fetch_count_of_users_that_entered_app_since_given_time, \
+    fetch_total_number_of_users, \
+    calculate_ratio_of_users_that_entered_app_today_versus_total_users, \
+    fetch_count_of_users_that_tried_saving, \
+    fetch_count_of_users_that_tried_withdrawing, \
+    calculate_ratio_of_users_that_saved_versus_users_that_tried_saving, \
     fetch_daily_metrics
 
 import main
 
 BIG_QUERY_DATASET_LOCATION = main.BIG_QUERY_DATASET_LOCATION
-FULL_TABLE_URL = main.FULL_TABLE_URL
+USER_BEHAVIOUR_TABLE_URL = main.USER_BEHAVIOUR_TABLE_URL
 SAVING_EVENT_TRANSACTION_TYPE = main.SAVING_EVENT_TRANSACTION_TYPE
 WITHDRAWAL_TRANSACTION_TYPE = main.WITHDRAWAL_TRANSACTION_TYPE
+FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY = main.FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY
+USER_OPENED_APP_EVENT_CODE = main.USER_OPENED_APP_EVENT_CODE
+EXTERNAL_EVENT_SOURCE = main.EXTERNAL_EVENT_SOURCE
+INTERNAL_EVENT_SOURCE = main.INTERNAL_EVENT_SOURCE
+ALL_USER_EVENTS_TABLE_URL = main.ALL_USER_EVENTS_TABLE_URL
+ENTERED_SAVINGS_FUNNEL_EVENT_CODE = main.ENTERED_SAVINGS_FUNNEL_EVENT_CODE
+ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE = main.ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE
+
+sample_total_users = 10
+sample_number_of_users = 2
 
 
 sample_user_id = "kig2"
 sample_age = 25
 sample_expected_users = [{ "age": sample_age }]
 sample_given_time = 1488138244035
-sample_period = sample_given_time
+sample_amount = 50000
 
 @pytest.fixture
 def mock_big_query():
     return Mock(spec=bigquery.Client())
+
+@pytest.fixture
+def mock_fetch_count_of_users_that_entered_app():
+    return Mock(spec=fetch_count_of_users_that_entered_app_since_given_time)
+
+@pytest.fixture
+def mock_fetch_total_number_of_users():
+    return Mock(spec=fetch_total_number_of_users)
+
+@pytest.fixture
+def mock_fetch_count_of_users_that_saved_since_given_time():
+    return Mock(spec=fetch_count_of_users_that_saved_since_given_time)
+
+@pytest.fixture
+def mock_fetch_count_of_users_that_tried_saving():
+    return Mock(spec=fetch_count_of_users_that_tried_saving)
+
+def test_convert_amount_from_hundredth_cent_to_whole_currency():
+    assert convert_amount_from_hundredth_cent_to_whole_currency(
+        sample_amount
+    ) ==  (float(sample_amount) * FACTOR_TO_CONVERT_HUNDREDTH_CENT_TO_WHOLE_CURRENCY)
 
 def test_extract_key_value_from_first_item_of_big_query_response_with_list():
     sample_response_list = [
@@ -65,8 +107,12 @@ def test_fetch_data_as_list_from_user_behaviour_table(mock_big_query):
 
 
 @patch('main.fetch_data_as_list_from_user_behaviour_table')
+@patch('main.extract_key_value_from_first_item_of_big_query_response')
+@patch('main.convert_amount_from_hundredth_cent_to_whole_currency')
 def test_fetch_total_amount_using_transaction_type(
-        fetch_from_table_patch
+        convert_amount_from_hundredth_cent_to_whole_currency_patch,
+        extract_key_value_from_first_item_of_big_query_response_patch,
+        fetch_from_table_patch,
 ):
 
     sample_config = {
@@ -81,7 +127,7 @@ def test_fetch_total_amount_using_transaction_type(
         from `{full_table_url}`
         where `transaction_type` = @transactionType
         and `time_transaction_occurred` >= @givenTime 
-        """.format(full_table_url=FULL_TABLE_URL)
+        """.format(full_table_url=USER_BEHAVIOUR_TABLE_URL)
     )
 
     sample_query_params = [
@@ -92,17 +138,19 @@ def test_fetch_total_amount_using_transaction_type(
     fetch_total_amount_using_transaction_type(given_transaction_type, sample_config)
 
     fetch_from_table_patch.assert_called_once_with(sample_query, sample_query_params)
+    extract_key_value_from_first_item_of_big_query_response_patch.assert_called_once()
+    convert_amount_from_hundredth_cent_to_whole_currency_patch.assert_called_once()
 
 
 @patch('main.fetch_total_amount_using_transaction_type')
-def test_fetch_total_saved_amount_during_period(
+def test_fetch_total_saved_amount_since_given_time(
         fetch_total_amount_using_transaction_type_patch
 ):
     sample_config = {
         "least_time_to_consider": sample_given_time,
     }
 
-    fetch_total_saved_amount_during_period(sample_period)
+    fetch_total_saved_amount_since_given_time(sample_given_time)
 
     fetch_total_amount_using_transaction_type_patch.assert_called_once_with(
         SAVING_EVENT_TRANSACTION_TYPE,
@@ -110,16 +158,221 @@ def test_fetch_total_saved_amount_during_period(
     )
 
 @patch('main.fetch_total_amount_using_transaction_type')
-def test_fetch_total_withdrawn_amount_during_period(
+def test_fetch_total_withdrawn_amount_given_time(
         fetch_total_amount_using_transaction_type_patch
 ):
     sample_config = {
         "least_time_to_consider": sample_given_time,
     }
 
-    fetch_total_withdrawn_amount_during_period(sample_period)
+    fetch_total_withdrawn_amount_given_time(sample_given_time)
 
     fetch_total_amount_using_transaction_type_patch.assert_called_once_with(
         WITHDRAWAL_TRANSACTION_TYPE,
         sample_config,
     )
+    
+@patch('main.fetch_data_as_list_from_user_behaviour_table')
+@patch('main.extract_key_value_from_first_item_of_big_query_response')
+def test_fetch_count_of_users_that_performed_transaction_type(
+        extract_key_value_from_first_item_of_big_query_response_patch,
+        fetch_from_table_patch,
+):
+
+    sample_config = {
+        "least_time_to_consider": sample_given_time,
+    }
+
+    given_transaction_type = WITHDRAWAL_TRANSACTION_TYPE
+
+    sample_query = (
+        """
+        select count(distinct(`user_id`)) as `countOfUsersThatPerformedTransactionType`
+        from `{full_table_url}`
+        where `transaction_type` = @transactionType
+        and `time_transaction_occurred` >= @givenTime
+        """.format(full_table_url=USER_BEHAVIOUR_TABLE_URL)
+    )
+
+    sample_query_params = [
+        bigquery.ScalarQueryParameter("transactionType", "STRING", given_transaction_type),
+        bigquery.ScalarQueryParameter("givenTime", "INT64", sample_given_time),
+    ]
+
+    fetch_count_of_users_that_performed_transaction_type(given_transaction_type, sample_config)
+
+    fetch_from_table_patch.assert_called_once_with(sample_query, sample_query_params)
+    extract_key_value_from_first_item_of_big_query_response_patch.assert_called_once()
+
+
+@patch('main.fetch_count_of_users_that_performed_transaction_type')
+def test_fetch_count_of_users_that_saved_since_given_time(
+        fetch_count_of_users_that_performed_transaction_type_patch
+):
+    sample_config = {
+        "least_time_to_consider": sample_given_time,
+    }
+
+    fetch_count_of_users_that_saved_since_given_time(sample_given_time)
+
+    fetch_count_of_users_that_performed_transaction_type_patch.assert_called_once_with(
+        SAVING_EVENT_TRANSACTION_TYPE,
+        sample_config,
+    )
+
+@patch('main.fetch_count_of_users_that_performed_transaction_type')
+def test_fetch_count_of_users_that_withdrew_since_given_time(
+        fetch_count_of_users_that_performed_transaction_type_patch
+):
+    sample_config = {
+        "least_time_to_consider": sample_given_time,
+    }
+
+    fetch_count_of_users_that_withdrew_since_given_time(sample_given_time)
+
+    fetch_count_of_users_that_performed_transaction_type_patch.assert_called_once_with(
+        WITHDRAWAL_TRANSACTION_TYPE,
+        sample_config,
+    )
+
+@patch('main.fetch_data_as_list_from_user_behaviour_table')
+@patch('main.extract_key_value_from_first_item_of_big_query_response')
+def test_fetch_count_of_users_that_performed_event(
+    extract_key_value_from_first_item_of_big_query_response_patch,
+    fetch_from_table_patch,
+):
+
+    sample_config = {
+        "event_type": USER_OPENED_APP_EVENT_CODE,
+        "source_of_event": EXTERNAL_EVENT_SOURCE,
+        "least_time_to_consider": sample_given_time,
+    }
+
+    sample_query = (
+        """
+        select count(distinct(`user_id`)) as `countOfUsersThatPerformedEvent`
+        from `{full_table_url}`
+        and `event_type` = @eventType
+        and `source_of_event` = @sourceOfEvent
+        and `time_transaction_occurred` >= @givenTime 
+        """.format(full_table_url=ALL_USER_EVENTS_TABLE_URL)
+    )
+
+    sample_query_params = [
+        bigquery.ScalarQueryParameter("givenTime", "INT64", sample_config["least_time_to_consider"]),
+        bigquery.ScalarQueryParameter("eventType", "STRING", sample_config["event_type"]),
+        bigquery.ScalarQueryParameter("sourceOfEvent", "STRING", sample_config["source_of_event"]),
+    ]
+
+    fetch_count_of_users_that_performed_event(sample_config)
+
+    fetch_from_table_patch.assert_called_once_with(sample_query, sample_query_params)
+    extract_key_value_from_first_item_of_big_query_response_patch.assert_called_once()
+
+
+@patch('main.fetch_count_of_users_that_performed_event')
+def test_fetch_count_of_users_that_entered_app_since_given_time(
+        fetch_count_of_users_that_performed_event_patch,
+):
+
+    sample_config = {
+        "event_type": USER_OPENED_APP_EVENT_CODE,
+        "source_of_event": EXTERNAL_EVENT_SOURCE,
+        "least_time_to_consider": sample_given_time,
+    }
+
+    fetch_count_of_users_that_entered_app_since_given_time(sample_given_time)
+
+    fetch_count_of_users_that_performed_event_patch.assert_called_once_with(sample_config)
+
+
+@patch('main.fetch_data_as_list_from_user_behaviour_table')
+@patch('main.extract_key_value_from_first_item_of_big_query_response')
+def test_fetch_total_number_of_users(
+        extract_key_value_from_first_item_of_big_query_response_patch,
+        fetch_from_table_patch,
+):
+
+    sample_query = (
+        """
+        select count(distinct(`user_id`)) as `totalNumberOfUsers`
+        from `{full_table_url}`
+        and `source_of_event` = @sourceOfEvent
+        """.format(full_table_url=ALL_USER_EVENTS_TABLE_URL)
+    )
+
+    sample_query_params = [
+        bigquery.ScalarQueryParameter("sourceOfEvent", "STRING", INTERNAL_EVENT_SOURCE),
+    ]
+
+    fetch_total_number_of_users()
+
+    fetch_from_table_patch.assert_called_once_with(sample_query, sample_query_params)
+    extract_key_value_from_first_item_of_big_query_response_patch.assert_called_once()
+
+@patch('main.fetch_count_of_users_that_performed_event')
+def test_fetch_count_of_users_that_tried_saving(
+        fetch_count_of_users_that_performed_event_patch,
+):
+
+    sample_config = {
+        "event_type": ENTERED_SAVINGS_FUNNEL_EVENT_CODE,
+        "source_of_event": EXTERNAL_EVENT_SOURCE,
+        "least_time_to_consider": sample_given_time,
+    }
+
+    fetch_count_of_users_that_tried_saving(sample_config["least_time_to_consider"])
+
+    fetch_count_of_users_that_performed_event_patch.assert_called_once_with(sample_config)\
+
+@patch('main.fetch_count_of_users_that_performed_event')
+def test_fetch_count_of_users_that_tried_withdrawing(
+        fetch_count_of_users_that_performed_event_patch,
+):
+
+    sample_config = {
+        "event_type": ENTERED_WITHDRAWAL_FUNNEL_EVENT_CODE,
+        "source_of_event": EXTERNAL_EVENT_SOURCE,
+        "least_time_to_consider": sample_given_time,
+    }
+
+    fetch_count_of_users_that_tried_withdrawing(sample_config["least_time_to_consider"])
+
+    fetch_count_of_users_that_performed_event_patch.assert_called_once_with(sample_config)
+
+
+def test_calculate_ratio_of_users_that_entered_app_today_versus_total_users(
+        mock_fetch_count_of_users_that_entered_app,
+        mock_fetch_total_number_of_users
+):
+
+    given_time = sample_given_time
+
+    main.fetch_count_of_users_that_entered_app_since_given_time = mock_fetch_count_of_users_that_entered_app
+    main.fetch_total_number_of_users = mock_fetch_total_number_of_users
+
+    mock_fetch_count_of_users_that_entered_app.return_value = sample_number_of_users
+    mock_fetch_total_number_of_users.return_value = sample_total_users
+
+    assert calculate_ratio_of_users_that_entered_app_today_versus_total_users(given_time) == (sample_number_of_users/sample_total_users)
+
+    mock_fetch_count_of_users_that_entered_app.assert_called_once_with(given_time)
+    mock_fetch_total_number_of_users.assert_called_once_with()
+
+def test_calculate_ratio_of_users_that_saved_versus_users_that_tried_saving(
+        mock_fetch_count_of_users_that_saved_since_given_time,
+        mock_fetch_count_of_users_that_tried_saving
+):
+
+    given_time = sample_given_time
+
+    main.fetch_count_of_users_that_saved_since_given_time = mock_fetch_count_of_users_that_saved_since_given_time
+    main.fetch_count_of_users_that_tried_saving = mock_fetch_count_of_users_that_tried_saving
+
+    mock_fetch_count_of_users_that_saved_since_given_time.return_value = sample_number_of_users
+    mock_fetch_count_of_users_that_tried_saving.return_value = sample_number_of_users
+
+    assert calculate_ratio_of_users_that_saved_versus_users_that_tried_saving(given_time) == (sample_number_of_users/sample_number_of_users)
+
+    mock_fetch_count_of_users_that_saved_since_given_time.assert_called_once_with(given_time)
+    mock_fetch_count_of_users_that_tried_saving.assert_called_once_with(given_time)
