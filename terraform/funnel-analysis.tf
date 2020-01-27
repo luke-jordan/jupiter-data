cffcf4c82b37e9b6819806336e9a260fb2eb0eb2
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2020-01-27 20:14:10 +0100

    Increase timeout of function to 420 seconds (7 minutes)

--- terraform/funnel-analysis.tf
+++ terraform/funnel-analysis.tf
@@ -4,7 +4,7 @@ resource "google_cloudfunctions_function" "funnel-analysis-function" {
   available_memory_mb = 128
   source_archive_bucket = "${var.gcp_bucket_prefix[terraform.workspace]}-funnel-analysis-bucket"
   source_archive_object = "funnel_analysis_${var.deploy_code_commit_hash}.zip"
-  timeout = 120
+  timeout = 420
   entry_point = "fetch_dropoff_and_recovery_users_count_given_list_of_steps"
   trigger_http = true
   runtime = "python37"
