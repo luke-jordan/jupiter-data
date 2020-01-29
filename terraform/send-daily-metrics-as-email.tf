9a120945b95be7cfa07cd4799c8a061e84bbef8e
Bolu Ajibawo <bolu.ajibawo@gmail.com>
2020-01-29 14:29:25 +0100

    Convert "admin contacts" from string to list
    The `notification service` expects contacts to be in the form list, on
    the other hand environment variables are stored as strings. Thus the
    need to convert string to list.

--- terraform/send-daily-metrics-as-email.tf
+++ terraform/send-daily-metrics-as-email.tf
@@ -11,6 +11,6 @@ resource "google_cloudfunctions_function" "send-daily-metrics-as-email-function"
   environment_variables = {
     FUNNEL_ANALYSIS_SERVICE_URL = "${terraform.workspace == "master" ? "https://europe-west1-jupiter-production-258809.cloudfunctions.net/funnel-analysis" : "https://us-central1-jupiter-ml-alpha.cloudfunctions.net/funnel-analysis"}"
     NOTIFICATION_SERVICE_URL = "${terraform.workspace == "master" ? "https://europe-west1-jupiter-production-258809.cloudfunctions.net/notification-service" : "https://us-central1-jupiter-ml-alpha.cloudfunctions.net/notification-service"}"
-    CONTACTS_TO_BE_NOTIFIED = "${terraform.workspace == "master" ? "['luke@plutosave.com', 'bolu@plutosave.com']" : "['luke@plutosave.com', 'avish@plutosave.com']"}"
+    CONTACTS_TO_BE_NOTIFIED = "${terraform.workspace == "master" ? "luke@plutosave.com, avish@plutosave.com" : "luke@plutosave.com, bolu@plutosave.com"}"
   }
 }
