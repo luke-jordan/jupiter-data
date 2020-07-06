f6005efee4e59a04dff6bce7f9a6647e7870501f
luke-grassroot <luke.jordan@grassroot.org.za>
2020-07-06 14:30:03 +0200

    Further CI work

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,7 @@
 # Storage bucket for function codes
 
 resource "google_storage_bucket" "function_code" {
-    name = "jupiter_cloudf_${terraform.workspace}"
+    name = "jupiter_cloudfunctions_${terraform.workspace}"
     location = var.gcp_default_continent[terraform.workspace]
 
     force_destroy = false
