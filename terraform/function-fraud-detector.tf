862fe70ef77e60ed29ab7c83ab79fa093180a1ee
luke-grassroot <luke.jordan@grassroot.org.za>
2020-08-22 18:45:53 +0200

    Adding some general calculations, adjusting fraud detection

--- terraform/function-fraud-detector.tf
+++ terraform/function-fraud-detector.tf
@@ -21,8 +21,20 @@ resource "google_cloudfunctions_function" "fraud-detector-function" {
         {
           "contactsToBeNotified": terraform.workspace == "master" ? ["luke@jupitersave.com", "avish@jupitersave.com"] : ["luke@jupitersave.com"]
           "GOOGLE_APPLICATION_CREDENTIALS": "service-account-credentials.json",
-          "AUTH_SERVICE_URL": terraform.workspace == "master" ? "https://production-auth.jupitersave.app/authentication" : "https://staging-auth.jupitersave.app/authentication"
+          "AUTH_SERVICE_ENABLED": true,
+          "AUTH_SERVICE_URL": terraform.workspace == "master" ? "https://master-auth.jupitersave.app/authentication" : "https://staging-auth.jupitersave.app/authentication"
         }
     )}" 
   }
+}
+
+# IAM entry for all users to invoke the function (using because GCP IAM = worst thing + little current risk in allowing general trigger)
+# obviously primary candidate for fixing/change, when we are able to generally fix the GCP IAM config
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
